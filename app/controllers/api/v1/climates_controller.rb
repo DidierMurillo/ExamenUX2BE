04e7928a168e27f7017e06a4d5cbf017d722a518
DidierMurillo <didiermurillo@unitec.edu>
2018-04-05 20:06:17 -0600

    Changes in controller

--- app/controllers/api/v1/climates_controller.rb
+++ app/controllers/api/v1/climates_controller.rb
@@ -4,5 +4,15 @@ module Api::V1
       @climates = Climate.all
       render json: @climates
     end
+
+    def create
+  		@climate = Climate.create(climate_params)
+  		render json: @climate
+	end
+	private
+	  def idea_params
+    	params.require(:climate).permit(:town,:temperature,:status)
+  	end
+
   end
 end
